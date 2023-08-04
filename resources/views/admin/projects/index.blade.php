b457e3dd44d81af61dcaf1659169601366f21d17
robertobenfini <roberto.benfini@gmail.com>
2023-08-04 15:19:56 +0200

    [ADD] stile index e admin con relative rotte

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,13 +1,38 @@
 @extends('layouts.admin')
 
 @section('content')
-
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h1>Ciao</h1>
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Slug</th>
+                            <th scope="col">Azioni</th>                            
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($projects as $project)  
+                            <tr>
+                                <th scope="row">{{ $project->id }}</th>
+                                <td>{{ $project->title }}</td>
+                                <td>{{ $project->slug }}</td>
+                                <td>
+                                    <a class="btn btn-info btn-sm" href="#"><i class="fas fa-eye"></i></a>
+                                    <a class="btn btn-warning btn-sm" href="#"><i class="fas fa-pen"></i></a>
+                                    <form class="d-inline-block" action="#" onsubmit="return confirm('Sei sicuro di voler cancellare questo fumetto?')" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
-
 @endsection
