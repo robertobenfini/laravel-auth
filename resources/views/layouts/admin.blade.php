2f4f583f1f5425444789e4701357c044cd8c4223
robertobenfini <roberto.benfini@gmail.com>
2023-08-04 15:44:35 +0200

    [style] font

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -96,18 +96,16 @@
                         <div class="sidebar">
                             <!-- Sidebar -->
                             <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-white">
-                                <div class="position-sticky">
-                                    <div class="list-group list-group-flush mt-4">
-                                        <a href="#" class="list-group-item list-group-item-action py-2 ripple" aria-current="true">
-                                            <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>Dashboard</span>
-                                        </a>
-                                        <a href="{{ route('admin.projects.index') }}" class="list-group-item py-2 ripple {{ Route::currentRouteName() === 'admin.projects.index' ? 'list-group-item-action list-group-item-danger' : ''}}">
-                                            <i class="fas fa-chart-area fa-fw me-3"></i><span>Projects</span>
-                                        </a>
-                                        <a href="#" class="list-group-item list-group-item-action py-2 ripple">
-                                            <i class="fa-solid fa-users me-3"></i><span>Users</span>
-                                        </a>
-                                    </div>
+                                <div class="list-group list-group-flush">
+                                    <a href="#" class="list-group-item list-group-item-action py-2 ripple" aria-current="true">
+                                        <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>Dashboard</span>
+                                    </a>
+                                    <a href="{{ route('admin.projects.index') }}" class="list-group-item py-2 ripple {{ Route::currentRouteName() === 'admin.projects.index' ? 'list-group-item-action list-group-item-danger' : ''}}">
+                                        <i class="fas fa-chart-area fa-fw me-3"></i><span>Projects</span>
+                                    </a>
+                                    <a href="#" class="list-group-item list-group-item-action py-2 ripple">
+                                        <i class="fa-solid fa-users me-3"></i><span>Users</span>
+                                    </a>
                                 </div>
                             </nav>
                         </div>
